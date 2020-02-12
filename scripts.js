9c65348bd63c6f1b39b4f91727b6f7c409e66eff
Muhammad <dulex51@hotmail.com>
2020-02-12 12:02:43 -0800

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+var video = document.getElementById("myVideo");
+
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
